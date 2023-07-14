4721fa3ca2b80bb5f779d4b2391adf1f5aed1298
Maxel-UP1 <angel.gonzalez04ptc.edu.co>
2023-07-13 22:23:13 -0500

    Se crea la primer version de los metodos del login

---
+++ EnhancedRegisterSystem/src/co/edu/uptc/controller/LoginController.java
@@ -0,0 +1,39 @@
+package co.edu.uptc.controller;
+
+
+public class LoginController{
+
+
+
+    //methods
+    public  boolean login(String nameUser, String password){
+
+
+        return false;
+    }
+
+    public boolean changePassword(String oldpassword, String newPassword){
+
+        return false;
+    }
+
+
+
+
+
+    public boolean logout(){
+
+
+        return false;
+    }
+    // no tengo claro si pedir el rol de una vez
+    public boolean signing(String nameUaser, String password){
+
+        return false;
+    }
+
+
+
+
+
+}
