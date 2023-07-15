e583c299e270f22aada9b3f28fe41b87ba21eb48
Nicolas-Sarmiento <nicolas.sarmiento@uptc.edu.co>
2023-07-14 21:48:36 -0500

    Account Controller Documentation

--- EnhancedRegisterSystem/src/co/edu/uptc/controller/AccountController.java
+++ EnhancedRegisterSystem/src/co/edu/uptc/controller/AccountController.java
@@ -5,7 +5,14 @@ import co.edu.uptc.model.Account;
 import java.util.HashSet;
 import java.util.List;
 
-
+/**
+ * La clase Account Controller permite gestionar una
+ * colección de cuentas que estaran disponibles en la
+ * ejecución del programa, así como la creación, eliminación
+ * y edición de contraseñas
+ * @author Nicolas Sarmiento : Nicolas-Sarmiento
+ * @version  1.0.0
+ */
 
 public class AccountController {
     private HashSet<Account> accounts;
@@ -27,6 +34,17 @@ public class AccountController {
         return true;
     }
 
+    /**
+     * El método AddAccount permite la creación y adición de
+     * una cuenta a la colección de cuentas, validando los paramétros
+     * de entrada
+     * @param id código miembro de la institución
+     * @param name Nombres del miembro de la institución
+     * @param lastName Apeellidos del miembro de la institución
+     * @param password Contraseña de la cuenta
+     * @param role Rol que tiene el miembro en la institución
+     * @return true si la cuenta fue añadida satisfactoriamente, false si ya existe o los parámetros son inválidos
+     */
     public boolean addAccount(String id, String name, String lastName, String password, String role){
         String username = "manuel.martinez";
         String email = "manuel.martinez@uptc.edu.co";
@@ -50,12 +68,28 @@ public class AccountController {
 
         return true;
     }
+
+    /**
+     * findAccount permite la busqueda de una cuenta a partir de
+     * usuario y contraseña
+     * @param username
+     * @param password
+     * @return Objeto de tipo Account, dicho objeto será nulo, si no es encontrado en la colección
+     */
     public Account findAccount(String username, String password){
         for (Account acc : this.accounts){
             if (acc.getUserName().equals(username) && acc.getPassword().equals(password)) return acc;
         }
         return null;
     }
+
+    /**
+     * Permite remover una cuenta de la colecciona a partir de
+     * usuario y contraseña
+     * @param username
+     * @param password
+     * @return true si la cuenta se eliminó, false si no se eliminó o no fue encontrada
+     */
     public boolean removeAccount(String username, String password){
         Account accountToRemove = this.findAccount(username, password);
 
@@ -63,6 +97,15 @@ public class AccountController {
 
         return this.accounts.remove(accountToRemove);
     }
+
+    /**
+     * Permite el cambio de contraseña de una cuenta a partir
+     * de usuario y contraseña
+     * @param username
+     * @param password contraseña antigua
+     * @param newPassword contraseña nueva
+     * @return true si el cambio fue satisfactorio, false si no se encontró la cuenta o la nueva contraseña es inválida
+     */
     public boolean setNewPassword(String username, String password, String newPassword){
         Account accountSetPassword = this.findAccount(username, password);
         if (accountSetPassword == null) return false;
@@ -85,7 +128,14 @@ public class AccountController {
         return false;
     }
 
-    public Account cloneAccount(Account account){
+    /**
+     * Pequeña utilidad que se utiliza en el cambio de contraseña,
+     * debido a que se requiere crear un nuevo objeto para que se genere
+     * un nuevo código hash en la colección
+     * @param account cuenta a ser clonada
+     * @return Account object con los atributos de la cuenta ingresada
+     */
+    private Account cloneAccount(Account account){
         return new Account(account.getId(), account.getName(), account.getLastname(), account.getUserName(), account.getPassword(), account.getRole(), account.getEmail());
     }
 }
