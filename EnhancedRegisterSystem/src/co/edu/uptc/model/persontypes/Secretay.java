d6843059fcf9906c8e2a70f53645c0ef6ef9cfa1
Nicolas-Sarmiento <nicolas.sarmiento@uptc.edu.co>
2023-07-20 18:44:45 -0500

    add: Person Types classes

---
+++ EnhancedRegisterSystem/src/co/edu/uptc/model/persontypes/Secretay.java
@@ -0,0 +1,9 @@
+package co.edu.uptc.model.persontypes;
+
+import co.edu.uptc.model.Person;
+
+public class Secretay  extends Person {
+    public Secretay(String id, String name, String lastname) {
+        super(id, name, lastname);
+    }
+}
