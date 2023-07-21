48e159a725f9791f0cd320fc357e9b3ccae142a0
Maxel-UP1 <angel.gonzalez04ptc.edu.co>
2023-07-20 20:47:00 -0500

    breakpoint line 30,31 on PersonController

--- EnhancedRegisterSystem/src/co/edu/uptc/test/testPerson.java
+++ EnhancedRegisterSystem/src/co/edu/uptc/test/testPerson.java
@@ -5,13 +5,19 @@ import co.edu.uptc.controller.PersonController;
 public class testPerson {
     public static void main(String[] args) {
         PersonController pctr = new PersonController();
+        LoginView lgview = new LoginView();
         //creo una nueva persona
         System.out.println( pctr.addPerson("01", "Samuel", "Gonzalez", "ADMINISTRATOR"));
+        
+
+
+
        //verifico que si se cree bien
        System.out.println(pctr.findPersonById("01").toString());
 
+        //creo una cuenta reperida
 
-
+        //System.out.println( pctr.addPerson("01", "Samuel", "Gonzalez", "ADMINISTRATOR"));
 
 
 
