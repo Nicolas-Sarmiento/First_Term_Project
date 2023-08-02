14d4dd0f41ad0065b01bf0d2b7555f20e8b5e6b3
Maxel-UP1 <angel.gonzalez04ptc.edu.co>
2023-08-02 12:11:25 -0500

    Createc class Course and modify the profesor class

--- EnhancedRegisterSystem/src/co/edu/uptc/model/persontypes/Professor.java
+++ EnhancedRegisterSystem/src/co/edu/uptc/model/persontypes/Professor.java
@@ -3,24 +3,18 @@ package co.edu.uptc.model.persontypes;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import co.edu.uptc.model.Forum;
+import co.edu.uptc.model.Course;
 
 import co.edu.uptc.model.Person;
 
 public class Professor extends Person {
-    private ArrayList<Forum> forumCreateds;
+    private ArrayList<Course> courses;
 
     public Professor(String id, String name, String lastname) {
         super(id, name, lastname);
-        this.forumCreateds = new ArrayList<>();
+        this.courses = new ArrayList<>();
     }
 
-    public ArrayList<Forum> getForumCreateds() {
-        return forumCreateds;
-    }
-
-    public void setForumCreateds(ArrayList<Forum> forumCreateds) {
-        this.forumCreateds = forumCreateds;
-    }
 
     
 }
