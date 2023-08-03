f6cdd70f48ca5ae5e06693e4f2307bb82188973d
EdwinMartinezGomez <edwin.martinez04@uptc.edu.co>
2023-08-02 20:00:17 -0500

    created class ForumController

---
+++ EnhancedRegisterSystem/src/co/edu/uptc/controller/ForumController.java
@@ -0,0 +1,19 @@
+package co.edu.uptc.controller;
+
+import java.util.ArrayList;
+
+public class ForumController {
+    private ArrayList<String> forums;
+    public ForumController(){
+        forums=new ArrayList<>();
+    }
+    public boolean createdForum(){
+        return false;
+    }
+    public boolean seeForum(){
+        return false;
+    }
+    public boolean deleteForum(){
+        return false;
+    }
+}
