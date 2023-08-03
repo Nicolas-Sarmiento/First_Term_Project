a07d62cc23b8493e4fe440b342e8f694e7cf11fd
Nicolas-Sarmiento <nicolas.sarmiento@uptc.edu.co>
2023-08-03 00:01:31 -0500

    (deleting) unnecesary classes

--- EnhancedRegisterSystem/src/co/edu/uptc/model/persontypes/Professor.java
+++ EnhancedRegisterSystem/src/co/edu/uptc/model/persontypes/Professor.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 import co.edu.uptc.model.Person;
 
 public class Professor extends Person {
-    private ArrayList<Course> courses;
+
 
     public Professor(String id, String name, String lastname) {
         super(id, name, lastname);
-        this.courses = new ArrayList<>();
+        
     }
 
 
